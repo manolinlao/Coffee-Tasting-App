736cf9f50dd0bc931e099cb9a9db373d29bcbe92
Manuel Lao <manolin@MacBook-Pro-de-Manuel.local>
2025-07-20 22:22:00 +0200

    inicio

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier', // 👈 Desactiva reglas que entren en conflicto con Prettier
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    // Aquí puedes agregar reglas personalizadas si quieres
+  },
+};
