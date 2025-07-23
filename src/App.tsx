07192894609f7271ef0aef0c4753b6bb47bb4bf4
Manuel Lao <manolin@MacBook-Pro-de-Manuel.local>
2025-07-23 20:05:37 +0200

    uninstall prime

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { Button } from 'primereact/button';
 import './App.css';
 
 export const App = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Text with tailwind</h1>
-      <Button label="holass"></Button>
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { PrimeReactProvider } from 'primereact/api';
-import 'primereact/resources/themes/md-light-indigo/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
 import './index.css';
 import { App } from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <PrimeReactProvider value={{ ripple: true }}>
-      <App />
-    </PrimeReactProvider>
+    <App />
   </StrictMode>
 );
