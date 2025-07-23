761c6e2782f75fb231b284dbb4a734796c7acb2d
Manuel Lao <manolin@MacBook-Pro-de-Manuel.local>
2025-07-23 19:58:08 +0200

    adding tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { PrimeReactProvider } from 'primereact/api';
+import 'primereact/resources/themes/md-light-indigo/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 import './index.css';
-import App from './App.tsx';
+import { App } from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <PrimeReactProvider value={{ ripple: true }}>
+      <App />
+    </PrimeReactProvider>
+  </StrictMode>
 );
