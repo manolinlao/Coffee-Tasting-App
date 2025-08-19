50fd8bfc3b3c05de1fa6426ac1577c64c6f56e7f
Manuel Lao <manolin@MacBook-Pro-de-Manuel.local>
2025-08-19 20:16:59 +0200

    added strinctmode

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-//import { StrictMode } from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { AppRouter } from './router/AppRouter';
 import './i18n/i18n';
@@ -6,7 +6,7 @@ import './index.css';
 import './api/linkModels';
 
 createRoot(document.getElementById('root')!).render(
-  //<StrictMode>
-  <AppRouter />
-  //</StrictMode>
+  <StrictMode>
+    <AppRouter />
+  </StrictMode>
 );
